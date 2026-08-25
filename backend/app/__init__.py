6f80236deaf8e26bd1fcec7b9facb664dc57b49d
Yuvaranjan-S <yuvaranjans@gmail.com>
2026-08-25 19:07:54 +0530

    Dynamically allow all Vercel domains via CORS allow_origin_regex

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -83,6 +83,7 @@ allowed_origins = sorted(list(set(origins)))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
